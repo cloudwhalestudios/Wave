98b729eab061bf81a502b7a5c0aea07857e6cdc5
NeonChicken LAPTOP <lcvandort@hotmail.com>
2019-05-26 20:47:59 +0200

    Added Meter Text
    Works and spawns the same as Meter Bars but measures the distance, puts it in text-form for user feedback

--- ProjectWave/Assets/Wave/Scripts/MeterManager.cs
+++ ProjectWave/Assets/Wave/Scripts/MeterManager.cs
@@ -7,6 +7,8 @@ public class MeterManager : MonoBehaviour
 {
     public GameObject PlayerObject;
     public GameObject MeterBar;
+    public Text MeterText;
+    public Canvas RenderCanvas;
 
     private int ObstacleIndex = 0;
     public int DistanceToNext = 50;
@@ -31,7 +33,19 @@ public class MeterManager : MonoBehaviour
 
     void CreateMeterBar()
     {
-        GameObject NewBar = Instantiate(MeterBar, new Vector3(0, ObstacleIndex * DistanceToNext), Quaternion.identity);
+        Vector3 NewMeterPos = new Vector3(0, ObstacleIndex * DistanceToNext);
+        GameObject NewBar = Instantiate(MeterBar, NewMeterPos, Quaternion.identity);
+
+        Text tempTextBox = Instantiate(MeterText, (NewMeterPos * 23), Quaternion.identity) as Text;
+
+        //Parent to the panel
+        tempTextBox.transform.SetParent(RenderCanvas.transform, false);
+        // Set the text box's text element font size and style:
+        // tempTextBox.fontSize = defaultFontSize;*/
+
+        //Set the text box's text element to the current textToDisplay:
+        tempTextBox.text = (ObstacleIndex * DistanceToNext) + "M";
+
         NewBar.transform.SetParent(transform);
         ObstacleIndex++;
     }
